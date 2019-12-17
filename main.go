94c8f2381444b92e93da58f3fef1f0f60520a06b
Jim van Kleef <jim.van.kleef@freiheit.com>
2019-12-17 16:24:20 +0100

    create pkg from probe

--- main.go
+++ main.go
@@ -14,51 +14,29 @@
 package main
 
 import (
-	"context"
-	"crypto/tls"
-	"crypto/x509"
 	"flag"
-	"fmt"
-	"io/ioutil"
+	hp "github.com/freiheit-com/grpc-health-probe/pkg/probe"
 	"log"
 	"os"
-	"os/signal"
 	"time"
-
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/credentials"
-	healthpb "google.golang.org/grpc/health/grpc_health_v1"
-	"google.golang.org/grpc/status"
-)
-
-var (
-	flAddr          string
-	flService       string
-	flUserAgent     string
-	flConnTimeout   time.Duration
-	flRPCTimeout    time.Duration
-	flTLS           bool
-	flTLSNoVerify   bool
-	flTLSCACert     string
-	flTLSClientCert string
-	flTLSClientKey  string
-	flTLSServerName string
-	flVerbose       bool
-)
-
-const (
-	// StatusInvalidArguments indicates specified invalid arguments.
-	StatusInvalidArguments = 1
-	// StatusConnectionFailure indicates connection failed.
-	StatusConnectionFailure = 2
-	// StatusRPCFailure indicates rpc failed.
-	StatusRPCFailure = 3
-	// StatusUnhealthy indicates rpc succeeded but indicates unhealthy service.
-	StatusUnhealthy = 4
 )
 
 func init() {
+	var (
+		flAddr          string
+		flService       string
+		flUserAgent     string
+		flConnTimeout   time.Duration
+		flRPCTimeout    time.Duration
+		flTLS           bool
+		flTLSNoVerify   bool
+		flTLSCACert     string
+		flTLSClientCert string
+		flTLSClientKey  string
+		flTLSServerName string
+		flVerbose       bool
+	)
+
 	log.SetFlags(0)
 	flag.StringVar(&flAddr, "addr", "", "(required) tcp host:port to connect")
 	flag.StringVar(&flService, "service", "", "service name to check (default: \"\")")
@@ -79,7 +57,7 @@ func init() {
 
 	argError := func(s string, v ...interface{}) {
 		log.Printf("error: "+s, v...)
-		os.Exit(StatusInvalidArguments)
+		os.Exit(int(hp.StatusInvalidArguments))
 	}
 
 	if flAddr == "" {
@@ -128,110 +106,14 @@ func init() {
 			log.Printf("  > server-name=%s", flTLSServerName)
 		}
 	}
+	clArgs = hp.RunProbeArgs{
+		FlAddr: flAddr, FlService: flService, FlUserAgent: flUserAgent, FlConnTimeout: flConnTimeout, FlRPCTimeout: flRPCTimeout,
+		FlTLS: flTLS, FlTLSNoVerify: flTLSNoVerify, FlTLSCACert: flTLSCACert, FlTLSClientCert: flTLSClientCert, FlTLSClientKey: flTLSClientKey, FlTLSServerName: flTLSServerName,
+		FlVerbose: flVerbose}
 }
 
-func buildCredentials(skipVerify bool, caCerts, clientCert, clientKey, serverName string) (credentials.TransportCredentials, error) {
-	var cfg tls.Config
-
-	if clientCert != "" && clientKey != "" {
-		keyPair, err := tls.LoadX509KeyPair(clientCert, clientKey)
-		if err != nil {
-			return nil, fmt.Errorf("failed to load tls client cert/key pair. error=%v", err)
-		}
-		cfg.Certificates = []tls.Certificate{keyPair}
-	}
-
-	if skipVerify {
-		cfg.InsecureSkipVerify = true
-	} else if caCerts != "" {
-		// override system roots
-		rootCAs := x509.NewCertPool()
-		pem, err := ioutil.ReadFile(caCerts)
-		if err != nil {
-			return nil, fmt.Errorf("failed to load root CA certificates from file (%s) error=%v", caCerts, err)
-		}
-		if !rootCAs.AppendCertsFromPEM(pem) {
-			return nil, fmt.Errorf("no root CA certs parsed from file %s", caCerts)
-		}
-		cfg.RootCAs = rootCAs
-	}
-	if serverName != "" {
-		cfg.ServerName = serverName
-	}
-	return credentials.NewTLS(&cfg), nil
-}
+var clArgs hp.RunProbeArgs
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	go func() {
-		sig := <-c
-		if sig == os.Interrupt {
-			log.Printf("cancellation received")
-			cancel()
-			return
-		}
-	}()
-
-	opts := []grpc.DialOption{
-		grpc.WithUserAgent(flUserAgent),
-		grpc.WithBlock()}
-	if flTLS {
-		creds, err := buildCredentials(flTLSNoVerify, flTLSCACert, flTLSClientCert, flTLSClientKey, flTLSServerName)
-		if err != nil {
-			log.Printf("failed to initialize tls credentials. error=%v", err)
-			os.Exit(StatusInvalidArguments)
-		}
-		opts = append(opts, grpc.WithTransportCredentials(creds))
-	} else {
-		opts = append(opts, grpc.WithInsecure())
-	}
-
-	if flVerbose {
-		log.Print("establishing connection")
-	}
-	connStart := time.Now()
-	dialCtx, cancel2 := context.WithTimeout(ctx, flConnTimeout)
-	defer cancel2()
-	conn, err := grpc.DialContext(dialCtx, flAddr, opts...)
-	if err != nil {
-		if err == context.DeadlineExceeded {
-			log.Printf("timeout: failed to connect service %q within %v", flAddr, flConnTimeout)
-		} else {
-			log.Printf("error: failed to connect service at %q: %+v", flAddr, err)
-		}
-		os.Exit(StatusConnectionFailure)
-	}
-	connDuration := time.Since(connStart)
-	defer conn.Close()
-	if flVerbose {
-		log.Printf("connection establisted (took %v)", connDuration)
-	}
-
-	rpcStart := time.Now()
-	rpcCtx, rpcCancel := context.WithTimeout(ctx, flRPCTimeout)
-	defer rpcCancel()
-	resp, err := healthpb.NewHealthClient(conn).Check(rpcCtx, &healthpb.HealthCheckRequest{Service: flService})
-	if err != nil {
-		if stat, ok := status.FromError(err); ok && stat.Code() == codes.Unimplemented {
-			log.Printf("error: this server does not implement the grpc health protocol (grpc.health.v1.Health)")
-		} else if stat, ok := status.FromError(err); ok && stat.Code() == codes.DeadlineExceeded {
-			log.Printf("timeout: health rpc did not complete within %v", flRPCTimeout)
-		} else {
-			log.Printf("error: health rpc failed: %+v", err)
-		}
-		os.Exit(StatusRPCFailure)
-	}
-	rpcDuration := time.Since(rpcStart)
-
-	if resp.GetStatus() != healthpb.HealthCheckResponse_SERVING {
-		log.Printf("service unhealthy (responded with %q)", resp.GetStatus().String())
-		os.Exit(StatusUnhealthy)
-	}
-	if flVerbose {
-		log.Printf("time elapsed: connect=%v rpc=%v", connDuration, rpcDuration)
-	}
-	log.Printf("status: %v", resp.GetStatus().String())
+	os.Exit(int(hp.RunProbe(clArgs)))
 }
